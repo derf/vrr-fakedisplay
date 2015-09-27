429d5b52623bed166808436fa0acf51e8a80e0ad
Daniel Friesel <derf@finalrewind.org>
2015-09-27 21:18:05 +0200

    perltidy...

--- index.pl
+++ index.pl
@@ -496,7 +496,7 @@ sub render_image {
 
 		my ( $line, $destination, $etr, undef ) = @{$d};
 
-		$line = substr($line, 0, 4);
+		$line = substr( $line, 0, 4 );
 
 		$png->draw_at( 0,  $line );
 		$png->draw_at( 25, $destination );
